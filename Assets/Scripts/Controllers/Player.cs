bfd31417437daf6219560c792347c1ffb98a7275
Osama Hussein <osamahusseinn2@gmail.com>
2024-10-07 19:01:44 -0400

    Stuck on drawing a circle
    I managed to get the line color to change when the player gets close to the enemy, but I'm struggling to draw a circle instead of lines that rotate around as if they circle around the player

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,10 +8,52 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    private Color detectionColor;
+
     // Update is called once per frame
     void Update()
     {
-
+        EnemyRadar(1.5f, 5);
     }
 
+    public void EnemyRadar(float radius, int circlePoints)
+    {
+        // Set the line color to red if the player is close to the enemy from a specific distance from the radius
+        if (Vector2.Distance(transform.position, enemyTransform.position) <= radius)
+        {
+            detectionColor = Color.red;
+        }
+
+        // Else set the line color to green
+        else
+        {
+            detectionColor = Color.green;
+        }
+
+        List<float> points = new List<float>();
+
+        for (int i = 0; i < circlePoints; i++)
+        {
+            points.Add(Random.Range(0, 360f));
+
+            // Create the first circle point
+            float firstAngleInRadians = points[0];
+
+            float firstPointX = Mathf.Cos(firstAngleInRadians);
+            float firstPointY = Mathf.Sin(firstAngleInRadians);
+
+            Vector3 firstPointResultant = new Vector3(firstPointX, firstPointY, 0.0f) * radius;
+
+            // Create the the rest of the circle points
+            float angleInRadians = points[i];
+
+            float x = Mathf.Cos(angleInRadians);
+            float y = Mathf.Sin(angleInRadians);
+
+            Vector3 resultant = new Vector3(x, y, 0) * radius;
+
+            Debug.DrawLine(transform.position + firstPointResultant, transform.position + resultant, detectionColor);
+        }
+    }
+    
 }
